444f49b50c6b8219dae3d2a21dc4a178bcb18b6b
python_00.py <leo2005222@gmail.com>
2020-04-11 14:17:40 +0900

    python 파일 생성

---
+++ pygame_00.py
@@ -0,0 +1,16 @@
+'''
+
+    2020.04.11
+    pygame 저장소 만들기
+
+    1. 'pygame' Project 생성하기
+    2. pygame module 설치하기
+        2-1. file / setting / pygame interpreter
+    3. git과 연동하기
+        3-1. VCS / import into version control / share project on github
+        3-2. venv 가상환경 연동 해제하기
+    4. pygame_00.py 파일 commit
+    5. pygame_00.py 파일 git repository / push
+    6. github site 확인
+
+'''
