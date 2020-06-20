40cc850c51df2564c4490aa9b8d2e1071a6f0dcd
101-14 <leo2005222>
2020-06-20 15:28:33 +0900

    스페이스-적 생성하기

--- space05.py
+++ space05.py
@@ -59,6 +59,22 @@ class Player(Ship):
         self.ship_img = YELLOW_SPACE_SHIP
 
 
+# 적 비행선 클래스
+class Enemy(Ship):
+    COLOR_MAP = {
+        'red'   : RED_SPACE_SHIP,
+        'blue'  : BLUE_SPACE_SHIP,
+        'green' : GREEN_SPACE_SHIP
+    }
+
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = self.COLOR_MAP[color]
+
+    def move(self, vel):
+        self.y += vel
+
+
 # 메인 함수
 def main():
     run = True
@@ -71,6 +87,10 @@ def main():
     player = Player(350, 600)
     player_vel = 5
 
+    enemies = []
+    wave_length = 5
+    enemy_vel = 2
+
     main_font = pygame.font.SysFont('comicsans', 50)
 
     # 화면 갱신 함수
@@ -82,6 +102,8 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (SCREEN_WIDTH - level_label.get_width() - 10, 10))
 
+        for enemy in enemies:
+            enemy.draw(WIN)
         player.draw(WIN)
         pygame.display.update()
 
@@ -89,10 +111,17 @@ def main():
         clock.tick(FPS)
         redraw_window()
 
+        if len(enemies) == 0:
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randint(50, 700),
+                              random.randint(-1000, -100),
+                              random.choice(['red', 'blue', 'green']))
+                enemies.append(enemy)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a] and player.x - player_vel >= 0:
@@ -104,7 +133,10 @@ def main():
         if keys[pygame.K_s] and player.y + player_vel <= SCREEN_HEIGHT - player.get_height():
             player.y += player_vel
 
-
+        for enemy in enemies:
+            enemy.move(enemy_vel)
+            if enemy.y + enemy_vel > SCREEN_HEIGHT:
+                enemies.remove(enemy)
 
 
 # 게임 실행
