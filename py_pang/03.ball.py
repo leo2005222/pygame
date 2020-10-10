11d7458af94083f8622e5274d560e6c7c63d2634
101-14 <leo2005222>
2020-10-10 15:51:41 +0900

    무기 표시

--- py_pang/03.ball.py
+++ py_pang/03.ball.py
@@ -6,6 +6,8 @@ import os
 # 파이게임 초기화
 pygame.init()
 
+# 프레임 설정
+clock = pygame.time.Clock()
 
 # 화면 생성 (640 X 480, PYPANG)
 screen_width = 640
@@ -31,15 +33,55 @@ character_width = character_size[0]
 character_height = character_size[1]
 character_x_pos = screen_width/2 - character_width/2
 character_y_pos = screen_height - stage_height - character_height
+character_speed = 0
+
+# 무기 이미지
+weapon = pygame.image.load(os.path.join(img_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+weapon_speed = 10
+
+# 무기 저장소
+weapons = []
 
 # 게임 루프
 running = True
 while running:
+    dt = clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_speed -= 5
+            elif event.key == pygame.K_RIGHT:
+                character_speed += 5
+            elif event.key == pygame.K_SPACE:
+                weapon_x_pos = character_x_pos + character_width//2 - weapon_width//2
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_speed = 0
+
+    # 게임 캐릭터 위치 업데이트
+    character_x_pos += character_speed
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    if character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 무기 위치 조정
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons]
+
+    # 천장에 닿은 무기 제거
+    weapons = [[w[0], w[1]]for w in weapons if w[1] > 0]
+
     screen.blit(bg, (0, 0))
+    for one in weapons:
+        screen.blit(weapon, (one[0], one[1]))
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
     pygame.display.update()
