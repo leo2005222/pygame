659ed34bcc3e5ff78a63514e8013f60a20e76db0
101-14 <leo2005222>
2020-10-24 14:33:03 +0900

    3. 캐릭터 이동

--- py_pang/05.final.py
+++ py_pang/05.final.py
@@ -15,6 +15,9 @@ import os
 # 파이게임 초기화
 pygame.init()
 
+# FPS (Frame Per Second)
+clock = pygame.time.Clock()
+
 screen_width = 640
 screen_height = 480
 screen_size = (screen_width, screen_height)
@@ -34,13 +37,29 @@ character_width = character_rect[0]
 character_height = character_rect[1]
 character_pos_x = screen_width//2 - character_width//2
 character_pos_y = screen_height - stage_height - character_height
+character_speed = 0
 
 # 게임 루프
 running = True
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_speed = -6
+            if event.key == pygame.K_RIGHT:
+                character_speed = 6
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_speed = 0
+
+    character_pos_x += character_speed
+    if character_pos_x < 0:
+        character_pos_x = 0
+    if character_pos_x > screen_width - character_width:
+        character_pos_x = screen_width - character_width
 
     # 화면 출력
     screen.blit(background_img, (0, 0))
