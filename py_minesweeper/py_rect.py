a600f31937cb034c5e6559705148525712cb4003
김민광 <leo2005222>
2020-12-12 14:46:17 +0900

    파이게임 도형 연습

---
+++ py_minesweeper/py_rect.py
@@ -0,0 +1,34 @@
+# 도형 연습 파일
+# py_rect.py
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    screen.fill((255, 255, 255))  # 화면에 흰색 채우기
+
+    # 빨간색 직사각형 그리기
+    pygame.draw.rect(screen, (255, 0, 0), (10, 20, 100, 50))
+
+    # 빨간색 직사각형 (굵기: 3)
+    pygame.draw.rect(screen, (255, 0, 0), (150, 10, 100, 30), 3)
+
+    # 녹색: x:100, y: 80 좌표에 가로 80, 세로 50 사각형 그리기 (단위: 픽셀)
+    pygame.draw.rect(screen, (0, 255, 0), ((100, 80), (80, 50)))
+    # 파란색: x:200, y: 60 좌표에 가로 140, 세로 80 직사각형
+    b_rect = pygame.Rect(200, 60, 140, 80)
+    pygame.draw.rect(screen, (0, 0, 255), b_rect)
+
+    # 노란색: x:30, y: 160 좌표에 가로 100, 세로 50 직사각형(R + G = Y)
+    y_rect = pygame.Rect((30, 160), (100, 50))
+    pygame.draw.rect(screen, (255, 255, 0), y_rect)
+
+    # 화면 업데이트
+    pygame.display.update()
